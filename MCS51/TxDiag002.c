c1d371e570b5de99afe12095eb70ebaa9e5ce491
Michael Sin <SpNovaDev@gmail.com>
2014-01-12 17:22:50 +0800

    Serial Port / Wireless diagnostic - sending ASCII codes working version

---
+++ MCS51/TxDiag002.c
@@ -0,0 +1,89 @@
+#include <stc89.h>
+
+void uartInit9600(void)
+{
+	// X'tal: 11.0495MHz
+	// 9600 Baud, 8 Bit Data, No Parity, 1 Stop Bit
+
+    TMOD = 0x20;
+    SCON = 0x40;
+    TH1 = 0xFD;
+    TL1 = TH1;
+    PCON = 0x00;
+    TR1 = 1;
+
+} /* uartInit9600 */
+
+void uartInit38400(void)
+{
+	// X'tal: 14.7456MHz
+	// 38400 Baud, 8 Bit Data, No Parity, 1 Stop Bit
+
+    TMOD = 0x20;
+    SCON = 0x40;
+    TH1 = 0xFF;
+    TL1 = TH1;
+    PCON = 0x00;
+	EA  = 1;			// Enable Interrupt
+	ES  = 1;		    // Enable Serial Port Interrupt
+    TR1 = 1;			// Start Timer1 as baud rate generator
+
+} /* uartInit38400 */
+
+void sendOneByte(unsigned char c)
+{
+    SBUF = c;
+    while(!TI);
+
+    TI = 0;
+} /* sendOneByte */
+
+void delayms(unsigned int t)   // 1ms delay: Îó²î -0.651041666667us
+{
+    unsigned int d;
+	unsigned char a,b;
+
+	for (d=0; d<t; d++)
+	{
+	    for(b=245;b>0;b--)
+	        for(a=1;a>0;a--);
+	}
+} /* delayms */
+
+void initBoard(void)
+{
+	PORT_Arduino 	 = 0xff;	// Set Port ready for Input from Arduino, set DataAck=1 (Standy)
+	PORT_Ctrl_Status = 0xff;	// Turn all LED Off and ready to read Key#2	status
+
+	WSN_RST			 = 0;		// Reset WSN-02/03 Wireless module
+	delayms(10);				// Delay 50ms
+	WSN_RST			 = 1;		// Normal Operation
+	delayms(10);				// Wait for WSN-02/03 ready
+	
+} /* initBoard */
+
+void main(void)
+{
+	unsigned char c;
+
+	initBoard();
+
+    uartInit38400();
+
+	for (;;)
+	{
+		P10 = 0;
+		P20 = 0;
+
+		delayms(500);
+
+		for (c=32; c<127; c++)
+			sendOneByte(c);
+
+		P10 = 1;
+		P20 = 1;
+
+		delayms(250);
+	}		
+} /* main */
+
